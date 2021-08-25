36d8808b0b5f94517a7b8ecc83af25cfdd537b4c
gherrerar <gabi.gh597@gmail.com>
2021-08-24 23:50:33 -0300

    Portfolio v2

--- js/script.js
+++ js/script.js
@@ -1,24 +1,100 @@
-let navbar = $(".navbar")
+$(function(){
+    let navbar = $(".navbar")
+    let pageAll = $(navbar).nextAll(":not(script)")
 
-setTimeout(function(){
-    $(navbar).css({
-        transition: "all calc(var(--transition-speed) * 2) ease-in-out",
-        transform: "translateY(0)",
-        opacity: "1"
+    //Index como Link anterior ao atual
+    let prevURL = document.referrer
+    console.log(prevURL)
+    if (prevURL.endsWith("index.html")) {
+        setTimeout(function(){
+            $(navbar).css({
+                transition: "all calc(var(--transition-speed) * 2) ease-in-out",
+                transform: "translateY(0)",
+                opacity: "1"
+            })
+        }, 1900)
+    }
+    //Link anterior ao atual diferente do Index
+    else {
+        $(navbar).css({
+            transform: "translateY(0)",
+            opacity: "1"
+        })
+        $(navbar).prev().find("#logo-img > path").css({
+            animation: "none",
+            fill: "rgba(0, 0, 0, 1)",
+            'stroke-dashoffset': "0"
+        })
+        setTimeout(()=> {
+            $(pageAll).css({
+                transition: "all var(--transition-speed) ease-in-out",
+                transform: "translateX(0)",
+                opacity: "1"
+            })
+        }, 200)
+    }
+
+    //Clique nos itens da Navbar
+    let navs = $(navbar).find(".nav-link")
+    navs.click(function(e){
+        e.preventDefault()
+        var href = $(this).attr("href")
+        var casesURL = ["index", "projetos", "sobre"]
+        let str = ''
+
+        var current = $(navs).filter(".selected")
+        current.removeClass("selected")
+        $(this).addClass("selected pop")
+
+        var rectNav = $(this)[0].getBoundingClientRect()
+        $(".main-nav span").css({
+            left: (rectNav.left - 75)
+        })
+
+        if ($(navs).parents(".navbar").hasClass("home") == false) {
+            var navText = $(this).attr("data-text")
+            document.styleSheets[0].deleteRule(0)
+            document.styleSheets[0].insertRule('.main-nav span::after { content: "' + navText + '" !important }', 0)
+        }
+
+
+        for (let i = 0; i < casesURL.length; i++) {
+
+            if (href.indexOf(casesURL[i]) > -1) {
+                $(pageAll).css({
+                    transform: "translateX(-8rem)",
+                    opacity: "0"
+                })
+                setTimeout(()=> {
+                    window.location.replace(href)
+                }, 500)
+            }
+        }
+        
     })
-}, 1900)
 
-let activeNav = $(navbar).find(".nav-link.selected")
-var rectNav = $(activeNav)[0].getBoundingClientRect()
-$(".main-nav span").css({
-    left: (rectNav.left - 75)
-})
+    $(window).on("resize", function(){
+        var current = $(navs).filter(".selected")
+        var rectNav = ''
 
-$(window).on("resize", function(){
-    rectNav = ''
+        rectNav = $(current)[0].getBoundingClientRect()
+        $(".main-nav span").css({
+            left: (rectNav.left - 75)
+        })
+    })
+    $(window).on("load", function(){
+        var current = $(navs).filter(".selected")
+        var rectNav = ''
 
-    var rectNav = $(activeNav)[0].getBoundingClientRect()
-    $(".main-nav span").css({
-        left: (rectNav.left - 75)
+        rectNav = $(current)[0].getBoundingClientRect()
+        $(".main-nav span").css({
+            left: (rectNav.left - 75)
+        })
+
+        if ($(navs).parents(".navbar").hasClass("home") == false) {
+            var navText = $(current).attr("data-text")
+            document.styleSheets[0].deleteRule(0)
+            document.styleSheets[0].insertRule('.main-nav span::after { content: "' + navText + '" !important }', 0)
+        }
     })
 })
